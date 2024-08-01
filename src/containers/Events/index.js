09256f30f10763990a493e160f8e5ebe9a897580
AmoGlobine <dennisaubryy@gmail.com>
2024-08-01 14:24:39 +0200

    updated events

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -14,16 +14,14 @@ const EventList = () => {
   const [type, setType] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const filteredEvents = (
-    data?.events || []
-  ).filter(event => !type || event.type === type);
+  const filteredEvents = data?.events?.filter(event => !type || event.type === type) || [];
   const paginatedEvents = filteredEvents.slice((currentPage - 1) * PER_PAGE, currentPage * PER_PAGE);
+  const pageNumber = Math.ceil(filteredEvents.length / PER_PAGE);
+  const typeList = Array.from(new Set(data?.events?.map(event => event.type)));
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
-  const pageNumber = Math.ceil(filteredEvents.length / PER_PAGE);
-  const typeList = Array.from(new Set(data?.events.map(event => event.type)));
   return (
     <>
        {error && <div>An error occured</div>}
